b8655117d27803c5808cdda181b4fbd79c4b25a2
Jim <the.jim.farrugia@gmail.com>
2019-08-26 21:50:20 +1000

    moved console.log to render method

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import SearchBar from './SearchBar';
-console.log(process.env.REACT_APP_UNSPLASH_KEY);
 
 export class App extends Component {
   onSearchSubmit(term) {
@@ -8,6 +7,7 @@ export class App extends Component {
   }
 
   render() {
+    console.log(process.env.REACT_APP_UNSPLASH_KEY);
     return (
       <div className="ui container" style={{ marginTop: 10 }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
