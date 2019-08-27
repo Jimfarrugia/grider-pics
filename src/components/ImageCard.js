41510d26d5e1291f86050f35b841995be2042715
Jim Farrugia <the,jim.farrugia@gmail.com>
2019-08-27 21:32:06 +1000

    get height of each image

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,13 +1,25 @@
 import React, { Component } from "react";
 
 export class ImageCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setSpans);
+  }
+
+  setSpans = () => {
+    console.log(this.imageRef.current.clientHeight);
+  };
+
   render() {
+    const { alt_description: description, urls } = this.props.image;
     return (
       <div>
-        <img
-          src={this.props.image.urls.regular}
-          alt={this.props.image.alt_description}
-        />
+        <img src={urls.regular} alt={description} ref={this.imageRef} />
       </div>
     );
   }
