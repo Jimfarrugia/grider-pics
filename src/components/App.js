88d32106976e402d9609c5b03ace5cb71f2ded67
Jim <the.jim.farrugia@gmail.com>
2019-08-27 15:58:31 +1000

    axios request success handler

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
+import axios from 'axios';
 import React, { Component } from 'react'
 import SearchBar from './SearchBar';
-import axios from 'axios';
 
 const UNSPLASH_API_KEY = process.env.REACT_APP_UNSPLASH_API_KEY;
 
 export class App extends Component {
-  onSearchSubmit(term) {
+  state = {
+    searchResults: [],
+  }
+
+  onSearchSubmit = term => {
     axios.get(
       'https://api.unsplash.com/search/photos', {
         params: { query: term },
@@ -14,9 +18,13 @@ export class App extends Component {
         }
       }
     )
-    .then(function (response) {
-      // handle success
-      console.log(response);
+    .then(response => {
+      // Success
+      const images = response.data.results.map(image => {
+        let { id, alt_description: description, urls: { regular: url } } = image;
+        return { id, description, url };
+      });
+      this.setState({searchResults: images});
     })
     .catch(function (error) {
       // handle error
