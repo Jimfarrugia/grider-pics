431209cf172a12bdb1556626b7fcb402627e8859
Jim <the.jim.farrugia@gmail.com>
2019-08-27 15:11:47 +1000

    (wip) axios call in App > onSearchSubmit()

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react'
 import SearchBar from './SearchBar';
+import axios from 'axios';
+
+const UNSPLASH_API_KEY = process.env.REACT_APP_UNSPLASH_API_KEY;
 
 export class App extends Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get(
+      'https://api.unsplash.com/search/photos', {
+        params: { query: term },
+        headers: {
+          Authorization: `Client-ID ${UNSPLASH_API_KEY}`
+        }
+      }
+    )
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+    });
   }
 
   render() {
