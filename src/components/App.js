a8e13d0c42634e23e32d17870a664b1f675344ac
Jim <the.jim.farrugia@gmail.com>
2019-08-27 17:00:51 +1000

    (I went rogue and) added ImageList component to show state.searchResults

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,16 @@ import SearchBar from './SearchBar';
 
 const UNSPLASH_API_KEY = process.env.REACT_APP_UNSPLASH_API_KEY;
 
+const ImageList = props => {
+  const searchResults = props.searchResults;
+  const images = searchResults.length < 1 ? null : searchResults.map(image => 
+    <li key={image.id}>
+      <img src={image.url} alt={image.description} />
+    </li>
+  );
+  return <ul>{images}</ul>;
+}
+
 export class App extends Component {
   state = {
     searchResults: [],
@@ -21,7 +31,7 @@ export class App extends Component {
     .then(response => {
       // Success
       const images = response.data.results.map(image => {
-        let { id, alt_description: description, urls: { regular: url } } = image;
+        let { id, alt_description: description, urls: { thumb: url } } = image;
         return { id, description, url };
       });
       this.setState({searchResults: images});
@@ -29,10 +39,11 @@ export class App extends Component {
     .catch(error => console.log(error));
   }
 
-  render() {
+  render() {    
     return (
       <div className="ui container" style={{ marginTop: 10 }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        <ImageList searchResults={this.state.searchResults} />
       </div>
     );
   }
